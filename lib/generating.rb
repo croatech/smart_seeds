fcb2603ae9fdd83de6284585eb83b8a2a0d42716
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-01-15 02:44:02 +0300

    add generating method

--- lib/generating.rb
+++ lib/generating.rb
@@ -13,5 +13,30 @@ module Generating
 
   def self.generate_value(column_type)
     raise ArgumentError, "There is no column type #{column_type}" unless Generating::AVAILABLE_TYPES.include?(column_type)
+
+    case column_type
+    when :binary
+      '0b100'
+    when :boolean
+      [true, false].sample
+    when :date
+      DateTime.now
+    when :datetime
+      DateTime.now
+    when :decimal
+      rand(6.6...666.0).round(2)
+    when :float
+      rand(6.6...666.0).round(2)
+    when :integer
+      rand(1...666)
+    when :string
+      'Heil'
+    when :text
+      'BIG text'
+    when :time
+      Time.now
+    else
+      raise ArgumentError
+    end
   end
 end
