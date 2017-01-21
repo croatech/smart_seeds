8f810dfe9c2cf6791802cfe5035270877f12c723
Vitaliy Fry <vitaliy.fry@gmail.com>
2017-01-21 05:03:45 +0300

    add recursively require files

--- lib/smart_seeds.rb
+++ lib/smart_seeds.rb
@@ -1,15 +1,4 @@
-require 'smart_seeds/performing'
-require 'smart_seeds/generator/base'
-require 'smart_seeds/generator/binary'
-require 'smart_seeds/generator/boolean'
-require 'smart_seeds/generator/date'
-require 'smart_seeds/generator/datetime'
-require 'smart_seeds/generator/decimal'
-require 'smart_seeds/generator/float'
-require 'smart_seeds/generator/integer'
-require 'smart_seeds/generator/string'
-require 'smart_seeds/generator/text'
-require 'smart_seeds/generator/time'
+Dir["#{File.dirname(__FILE__)}/smart_seeds/**/*.rb"].each {|file| require file }
 
 module SmartSeeds
   def self.call(model, attrs={})
